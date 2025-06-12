e4b4c435d3e53598dd8391b2f3f74ea911e5c423
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-12 10:08:35 +0900

    중요사항 몇가지 변경

--- src/main/java/com/welab/k8s_backend_user/remote/alim/RemoteAlimService.java
+++ src/main/java/com/welab/k8s_backend_user/remote/alim/RemoteAlimService.java
@@ -4,9 +4,9 @@ import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "RemoteAlimService",
+@FeignClient(name = "backend-alim",
         url = "http://k8s-backend-alim-service:8080",
-        path = "/backend/alim/v1")
+        path = "/api/alim/v1")
 public interface RemoteAlimService {
     @GetMapping(value = "/hello")
     public ApiResponseDto<String> hello();
