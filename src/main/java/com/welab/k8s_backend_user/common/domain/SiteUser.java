f4db2d1890df2ab4d39b3d2d5f395ca119b37109
jangseungwon08 <jangseungwon9819@gmail.com>
2025-06-12 13:05:26 +0900

    DB연결 추가

---
+++ src/main/java/com/welab/k8s_backend_user/common/domain/SiteUser.java
@@ -0,0 +1,37 @@
+package com.welab.k8s_backend_user.common.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Slf4j
+@Table(name = "site_user")
+public class SiteUser {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Column(name = "user_id", unique = true, nullable = false)
+    @Getter @Setter
+    private String userId;
+
+    @Column(name = "password", nullable = false)
+//    null이 있을 수 있는 엔티티 칼럼이면 Optional로 주는 것이 바람직
+    @Getter @Setter
+    private String password;
+
+    @Column(name = "phone_number", nullable = false)
+    @Getter @Setter
+    private String phoneNumber;
+
+//    flage를 두는 것이 더 바람직하다.
+    @Column(name = "deleted", nullable = false)
+    @Getter @Setter
+    private Boolean deleted = false; //flag를 하나 두는 방법
+
+}
--- src/main/java/com/welab/k8s_backend_user/remote/alim/RemoteAlimService.java
+++ src/main/java/com/welab/k8s_backend_user/remote/alim/RemoteAlimService.java
@@ -4,6 +4,9 @@ import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
+//유레카에서 서비스 알림 목록을 가져왔을 때
+//name은 규칙이 따로 없다. (단지 app.yml에다가 특정 feignclient에 등록하고 싶을 때 이름을 써주면된다.)
+//url은 서비스 명칭이다. -> 서비스 자체가 추상화기때문에 다음과 같이 포트번호와 함께 써주면되낟.
 @FeignClient(name = "backend-alim",
         url = "http://k8s-backend-alim-service:8080",
         path = "/api/alim/v1")
